bb2b8a950bfcb7a4d27ea60db382dc93efc93be9
Steven Yates <stevenyates0705@gmail.com>
2025-03-24 16:05:43 -0500

    Replace tabs with spaces and fix formatting on many files

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -1,45 +1,45 @@
-
 function has_value(t, val)
-	for i, v in ipairs(t) do
-		if v == val then return 1 end
-	end
-	return 0
+    for i, v in ipairs(t) do
+        if v == val then
+            return 1
+        end
+    end
+    return 0
 end
 
 function has(item, amount)
-	local count = Tracker:ProviderCountForCode(item)
-	amount = tonumber(amount)
-	if not amount then
-		return count > 0
-	else
-		return count >= amount
-	end
+    local count = Tracker:ProviderCountForCode(item)
+    amount = tonumber(amount)
+    if not amount then
+        return count > 0
+    else
+        return count >= amount
+    end
 end
 
 function progCount(code)
-	return Tracker:FindObjectForCode(code).AcquiredCount
+    return Tracker:FindObjectForCode(code).AcquiredCount
 end
 
 function dump_table(o, depth)
-	if depth == nil then
-		depth = 0
-	end
-	if type(o) == 'table' then
-		local tabs = ('\t'):rep(depth)
-		local tabs2 = ('\t'):rep(depth + 1)
-		local s = '{\n'
-		for k, v in pairs(o) do
-			if type(k) ~= 'number' then
-				k = '"' .. k .. '"'
-			end
-			s = s .. tabs2 .. '[' .. k .. '] = ' .. dump_table(v, depth + 1) .. ',\n'
-		end
-		return s .. tabs .. '}'
-	else
-		return tostring(o)
-	end
+    if depth == nil then
+        depth = 0
+    end
+    if type(o) == 'table' then
+        local tabs = ('\t'):rep(depth)
+        local tabs2 = ('\t'):rep(depth + 1)
+        local s = '{\n'
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. tabs2 .. '[' .. k .. '] = ' .. dump_table(v, depth + 1) .. ',\n'
+        end
+        return s .. tabs .. '}'
+    else
+        return tostring(o)
+    end
 end
-
 
 function toggle_johto(code)
     if has("johto_only_off") then
